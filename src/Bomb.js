7ec28d06d73b8a3ca8959de2ab41dd4cbabf0e44
lizburkechi <liz.burke.chi@gmail.com>
2021-05-19 17:30:54 -0500

    commit tree dfcf70032705a9da6927bcfd7448fba13ba9083d

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,11 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    render() {
+        return (
+            <h1></h1>
+        )
+    }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,11 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+       render() {
+        return (
+            <h1>yo</h1>
+        )
+    }
+}
+
+export default ImageSlider;
